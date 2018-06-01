742646018c1f803359c917ea376dac138ccc5687
Pedro A.Moreno <pedabraham@gmail.com>
2018-06-01 08:46:24 -0500

    .

--- selector/selector.ino
+++ selector/selector.ino
@@ -1,10 +1,16 @@
 void setup() {
-  for (int i = 0; i < 2; i++) {
-    pinMode(i,OUTPUT);
-  }
+        for (int i = 0; i < 2; i++) {
+                pinMode(i,OUTPUT);
+        }
 }
 
 void loop() {
-  int señal=0;
-  case digitalWrite(señal,HIGH);
+        int señal=0;
+        digitaWrite(señal,HIGH);
+        for (int i = 0; i < 3; i++) {
+                if(i!=señal) {
+                        digitaWrite(i,LOW);
+                }
+        }
+
 }
